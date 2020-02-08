b36aaf8628428fde022f5d4940a0b68da9e84134
Kshitij Jain <jainkshitij99@gmail.com>
2020-02-09 03:20:52 +0530

    try to merge

---
+++ app/src/main/java/com/example/winter/MapsActivity.java
@@ -0,0 +1,172 @@
+package com.example.winter;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+
+    private GoogleMap mMap;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
+        {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED)
+            {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                centerMapOnLocation(lastKnownLocation, "Your Location!");
+            }
+        }
+    }
+
+    public void centerMapOnLocation (Location location, String titlee)
+    {
+        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        //mMap.clear();
+        if(titlee == "Your Location!")
+            mMap.addMarker(new MarkerOptions().position(userLocation).title(titlee));
+        else
+            mMap.addMarker(new MarkerOptions().position(userLocation).title(titlee).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 12));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnMapLongClickListener(this);
+
+        Intent intent = getIntent();
+        if(intent.getIntExtra("placeNumber", 0)==0)
+        {
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    //centerMapOnLocation(location, "Your Location!");
+                }
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                }
+                @Override
+                public void onProviderEnabled(String provider) {
+                }
+                @Override
+                public void onProviderDisabled(String provider) {
+                }
+            };
+
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+            {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if(lastKnownLocation!=null) {
+                    centerMapOnLocation(lastKnownLocation, "Your Location!");
+                }
+                else{
+//                    LatLng sydney = new LatLng(-34, 151);
+//                    centerMapOnLocation(sydney,"your location");
+//                    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//                    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                    Location loc=new Location(LocationManager.GPS_PROVIDER);
+                    centerMapOnLocation(loc,"Your Location");
+                }
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+
+        }
+        else
+        {
+            //mMap.clear();
+            Location placeLocation = new Location(LocationManager.GPS_PROVIDER);
+            placeLocation.setLatitude(MarkUnsafe.locations.get(intent.getIntExtra("placeNumber", 0)).latitude);
+            placeLocation.setLongitude(MarkUnsafe.locations.get(intent.getIntExtra("placeNumber", 0)).longitude);
+
+            centerMapOnLocation(placeLocation, MarkUnsafe.places.get(intent.getIntExtra("placeNumber", 0)));
+            mMap.addMarker(new MarkerOptions().position(MarkUnsafe.locations.get(intent.getIntExtra("placeNumber", 0))).title(MarkUnsafe.places.get(intent.getIntExtra("placeNumber", 0))).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+            LatLng userLocation = new LatLng(placeLocation.getLatitude(), placeLocation.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+        }
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        String address = "";
+        try {
+            List<Address> listAddresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            if(listAddresses!=null && listAddresses.size()>0)
+            {
+                if(listAddresses.get(0).getThoroughfare()!=null)
+                {
+                    if(listAddresses.get(0).getSubThoroughfare()!=null)
+                    {
+                        address += listAddresses.get(0).getSubThoroughfare() + " ";
+                    }
+                    address += listAddresses.get(0).getThoroughfare();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(address == "")
+        {
+            address = "Latitude:" + Double.toString(latLng.latitude) + " Longitide:" + Double.toString(latLng.longitude);
+        }
+        Log.i("Location: ", address);
+        mMap.addMarker(new MarkerOptions().position(latLng).title(address).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+        MarkUnsafe.places.add(address);
+        MarkUnsafe.locations.add(latLng);
+        MarkUnsafe.arrayAdapter.notifyDataSetChanged();
+
+        Toast.makeText(this,  address + " : Marked Unsafe!", Toast.LENGTH_SHORT).show();
+//        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/winter/closedGroup.java
@@ -0,0 +1,147 @@
+package com.example.winter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import static com.example.winter.MainActivity.dref;
+import static com.example.winter.MainActivity.dref;
+import static com.example.winter.MainActivity.dref;
+
+public class closedGroup extends AppCompatActivity {
+    ArrayList<String> contactList = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
+    EditText name;
+    EditText phone;
+    ListView listView;
+    Button addb;
+    Button bbutton;
+    Contacts user;
+    Contacts user1;
+    boolean is_added;
+    public void setValues() {
+        user.setName(name.getText().toString());
+        user.setPhoneNo(phone.getText().toString());
+    }
+    public void back(View view){
+        viewVisible();
+
+    }
+    public void viewVisible(){
+        name.setVisibility(View.INVISIBLE);
+        phone.setVisibility(View.INVISIBLE);
+        addb.setVisibility(View.INVISIBLE);
+        listView.setVisibility(View.VISIBLE);
+        bbutton.setVisibility(View.INVISIBLE);
+    }
+
+    public void add(View view) {
+
+        user = new Contacts();
+        setValues();
+        is_added=false;
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(addb.getWindowToken(), 0);
+        arrayAdapter.clear();
+//        if(name.getText().toString().equals("")|| phone.getText().toString().equals(""))
+//        {
+//            viewVisible();
+//            return;
+//        }
+        dref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(!is_added) {
+
+                        dref.push().setValue(user);
+                        contactList.add(user.getName() + " : " + user.getPhoneNo());
+                        contactList.set(0, "enter new contact");
+                        arrayAdapter.notifyDataSetChanged();
+
+                    is_added=true;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        viewVisible();
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_closed_group);
+        Intent intent = getIntent();
+        listView = findViewById(R.id.contacts);
+        bbutton=findViewById(R.id.button2);
+        name = findViewById(R.id.editText3);
+        phone = findViewById(R.id.editText4);
+        addb = findViewById(R.id.button);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, contactList);
+
+        contactList.clear();
+        contactList.add("enter new contact");
+        dref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    user1 = ds.getValue(Contacts.class);
+                    if (user1.getName() != null && user1.getPhoneNo() != null) {
+                        contactList.add(user1.getName() + " : " + user1.getPhoneNo());
+                        arrayAdapter.notifyDataSetChanged();
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    name.setVisibility(View.VISIBLE);
+                    phone.setVisibility(View.VISIBLE);
+                    listView.setVisibility(View.INVISIBLE);
+                    addb.setVisibility(View.VISIBLE);
+                    name.getText().clear();
+                    phone.getText().clear();
+                    bbutton.setVisibility(View.VISIBLE);
+
+                }
+            }
+        });
+
+    }
+}
+
+
